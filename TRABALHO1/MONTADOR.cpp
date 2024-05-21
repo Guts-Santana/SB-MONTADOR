d4bae7d4d02fa3396be8094fd6dd9dc8f90393fa
yantavares <yantdo1@gmail.com>
2024-05-21 18:46:41 -0300

    organized files

---
+++ TRABALHO1/MONTADOR.cpp
@@ -0,0 +1,348 @@
+#include "MONTADOR.hpp"
+
+Assembler::Assembler::Assembler()
+    : line_counter(1), PC(0), should_be_linked(false),
+      opcodes{"ADD", "SUB", "MUL", "DIV", "JMP", "JMPN", "JMPP", "JMPZ", "COPY", "LOAD", "STORE", "INPUT", "OUTPUT", "STOP"},
+      opcode_values{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"}
+{
+}
+
+void Assembler::Assembler::ReadFile(const std::string &filename)
+{
+    std::string line;
+    std::string word;
+    std::vector<std::string> parsed_line;
+
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "PRE file not found" << std::endl;
+        return;
+    }
+
+    while (getline(file, line))
+    {
+        std::istringstream iss(line);
+        parsed_line.clear();
+
+        while (iss >> word)
+        {
+            parsed_line.push_back(word);
+        }
+
+        if (UpdateUsageTable(parsed_line))
+        {
+            continue;
+        }
+
+        parsed_line = FindLabel(parsed_line);
+
+        if (parsed_line[0] == "BEGIN")
+        {
+            should_be_linked = true;
+            line_counter++;
+            continue;
+        }
+
+        if (parsed_line[0] == "END")
+        {
+            line_counter++;
+            continue;
+        }
+
+        if (parsed_line[0] == "EXTERN")
+        {
+            line_counter++;
+            usage_table[labels.back()].push_back(-1);
+            continue;
+        }
+
+        if (parsed_line[0] == "PUBLIC")
+        {
+            line_counter++;
+            definition_table[parsed_line[1]].push_back(-1);
+            continue;
+        }
+
+        if (parsed_line[0] == "COPY")
+        {
+            parsed_line = ProcessCopyInstruction(parsed_line);
+        }
+
+        WriteProgram(parsed_line, should_be_linked);
+
+        if (parsed_line.size() > 1 || (parsed_line.size() == 1 && parsed_line[0].back() != ':'))
+        {
+            if (parsed_line.size() != 1 && parsed_line[0].back() != ':')
+            {
+                PC += parsed_line.size();
+            }
+            else
+            {
+                PC++;
+            }
+        }
+
+        line_counter++;
+    }
+}
+
+bool Assembler::Assembler::UpdateUsageTable(const std::vector<std::string> &line)
+{
+    if (line[0].back() == ':')
+    {
+        std::string temp = line[0];
+        temp.pop_back();
+
+        if (definition_table.find(temp) != definition_table.end())
+        {
+            std::vector<std::string> temp_line = line;
+
+            while (temp_line[0].back() == ':')
+            {
+                temp_line.erase(temp_line.begin());
+            }
+
+            if (temp_line[0] == "CONST")
+            {
+                definition_table[temp].clear();
+                definition_table[temp].push_back(std::stoi(temp_line[1]));
+            }
+            else if (temp_line[0] == "SPACE")
+            {
+                definition_table[temp].clear();
+                definition_table[temp].push_back(0);
+            }
+            else
+            {
+                definition_table[temp].push_back(PC);
+            }
+
+            return true;
+        }
+    }
+    return false;
+}
+
+void Assembler::Assembler::WriteFile(const std::string &filename)
+{
+    std::string output_filename = filename.substr(0, filename.size() - 3) + "obj";
+    std::ofstream file(output_filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open file for writing" << std::endl;
+        return;
+    }
+
+    if (should_be_linked)
+    {
+        file << "USO\n";
+        for (const auto &[key, value] : usage_table)
+        {
+            file << key << " ";
+            for (int v : value)
+            {
+                if (v != -1)
+                {
+                    file << v << " ";
+                }
+            }
+            file << '\n';
+        }
+
+        file << "DEF\n";
+        for (const auto &[key, value] : definition_table)
+        {
+            file << key << " ";
+            for (int v : value)
+            {
+                if (v != -1)
+                {
+                    file << v << " ";
+                }
+            }
+            file << '\n';
+        }
+
+        file << "\nREAL\n";
+        for (int val : relocation_table)
+        {
+            file << val;
+        }
+        file << '\n';
+    }
+
+    for (const std::string &i : program)
+    {
+        std::cout << i << ' ';
+        file << i << ' ';
+    }
+    file.close();
+}
+
+std::vector<std::string> Assembler::Assembler::FindLabel(std::vector<std::string> line)
+{
+    if (line[0].back() == ':')
+    {
+        std::string label = line[0];
+        label.pop_back();
+
+        auto it = std::find(labels.begin(), labels.end(), label);
+        if (it != labels.end())
+        {
+            auto undef_it = std::find(undefined_labels.begin(), undefined_labels.end(), label);
+            if (undef_it != undefined_labels.end())
+            {
+                int pos = std::distance(labels.begin(), it);
+                program[std::stoi(label_addresses[pos])] = std::to_string(PC);
+                label_addresses[pos] = std::to_string(PC);
+                pos = std::distance(undefined_labels.begin(), undef_it);
+                undefined_labels[pos] = "0";
+                line.erase(line.begin());
+                return line;
+            }
+            else
+            {
+                throw std::invalid_argument("Semantic Error: LABEL REDEFINED");
+            }
+        }
+        else if (line[1] != "SPACE" && line[1] != "CONST")
+        {
+            labels.push_back(label);
+            label_addresses.push_back(std::to_string(PC));
+            line.erase(line.begin());
+        }
+    }
+    return line;
+}
+
+void Assembler::Assembler::WriteProgram(const std::vector<std::string> &line, bool shouldBeLinked)
+{
+    int position;
+    for (size_t i = 0; i < line.size(); ++i)
+    {
+        if (shouldBeLinked)
+        {
+            auto it = usage_table.find(line[i]);
+            if (it != usage_table.end())
+            {
+                relocation_table.push_back(1);
+                usage_table[line[i]].push_back(PC + static_cast<int>(i) + 1);
+                program.push_back("0");
+                continue;
+            }
+        }
+
+        auto opcode_it = std::find(opcodes.begin(), opcodes.end(), line[i]);
+        if (opcode_it != opcodes.end())
+        {
+            if (shouldBeLinked)
+            {
+                relocation_table.push_back(0);
+            }
+            position = std::distance(opcodes.begin(), opcode_it);
+            program.push_back(opcode_values[position]);
+            continue;
+        }
+
+        auto label_it = std::find(labels.begin(), labels.end(), line[i]);
+        if (label_it != labels.end())
+        {
+            if (shouldBeLinked)
+            {
+                relocation_table.push_back(1);
+            }
+            position = std::distance(labels.begin(), label_it);
+            program.push_back(label_addresses[position]);
+            continue;
+        }
+
+        auto symbol_it = std::find(symbols.begin(), symbols.end(), line[i]);
+        if (symbol_it != symbols.end())
+        {
+            relocation_table.push_back(1);
+            position = std::distance(symbols.begin(), symbol_it);
+            program.push_back(symbol_addresses[position]);
+            symbol_addresses[position] = std::to_string(PC + static_cast<int>(i));
+            continue;
+        }
+
+        if (line[0].back() == ':')
+        {
+            if (shouldBeLinked)
+            {
+                relocation_table.push_back(0);
+            }
+            WriteSymbols(line);
+            break;
+        }
+        else
+        {
+            if (line[0] != "JMP" && line[0] != "JMPP" && line[0] != "JMPZ" && line[0] != "JMPN")
+            {
+                if (shouldBeLinked)
+                {
+                    relocation_table.push_back(1);
+                }
+                symbols.push_back(line[i]);
+                symbol_addresses.push_back(std::to_string(PC + static_cast<int>(i)));
+                program.push_back("-1");
+            }
+            else
+            {
+                labels.push_back(line[i]);
+                label_addresses.push_back(std::to_string(PC + static_cast<int>(i)));
+                undefined_labels.push_back(line[i]);
+                program.push_back("-1");
+            }
+        }
+    }
+}
+
+void Assembler::Assembler::WriteSymbols(const std::vector<std::string> &line)
+{
+    std::string label = line[0];
+    label.pop_back();
+    if (line.size() != 2 && line.size() != 3)
+    {
+        throw std::invalid_argument("Syntax Error");
+    }
+
+    auto symbol_it = std::find(symbols.begin(), symbols.end(), label);
+    if (symbol_it != symbols.end())
+    {
+        int position = std::distance(symbols.begin(), symbol_it);
+        if (line[1] == "CONST")
+        {
+            program.push_back(line[2]);
+        }
+        else if (line[1] == "SPACE")
+        {
+            program.push_back("0");
+        }
+        else
+        {
+            throw std::invalid_argument("Error");
+        }
+
+        int pos = std::stoi(symbol_addresses[position]);
+        while (program[pos] != "-1")
+        {
+            int temp = std::stoi(program[pos]);
+            program[pos] = std::to_string(PC);
+            pos = temp;
+        }
+        program[pos] = std::to_string(PC);
+    }
+}
+
+std::vector<std::string> Assembler::Assembler::ProcessCopyInstruction(std::vector<std::string> line)
+{
+    std::stringstream ss(line[1]);
+    std::string first_symbol, second_symbol;
+    std::getline(ss, first_symbol, ',');
+    std::getline(ss, second_symbol, ',');
+    line[1] = first_symbol;
+    line.push_back(second_symbol);
+    return line;
+}
---
+++ TRABALHO1/MONTADOR.hpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+#include <stdexcept>
+#include <unordered_map>
+
+namespace Assembler
+{
+    class Assembler
+    {
+    public:
+        Assembler();
+        void ReadFile(const std::string &filename);
+        void WriteFile(const std::string &filename);
+
+    private:
+        int line_counter;
+        int PC;
+        bool should_be_linked;
+        std::vector<std::string> labels;
+        std::vector<std::string> label_addresses;
+        std::vector<std::string> program;
+        std::vector<std::string> symbols;
+        std::vector<std::string> symbol_addresses;
+        std::vector<std::string> opcodes;
+        std::vector<std::string> opcode_values;
+        std::vector<std::string> undefined_labels;
+        std::unordered_map<std::string, std::vector<int>> usage_table;
+        std::unordered_map<std::string, std::vector<int>> definition_table;
+        std::vector<int> relocation_table;
+
+        bool UpdateUsageTable(const std::vector<std::string> &line);
+        std::vector<std::string> FindLabel(std::vector<std::string> line);
+        void WriteProgram(const std::vector<std::string> &line, bool shouldBeLinked = false);
+        void WriteSymbols(const std::vector<std::string> &line);
+        std::vector<std::string> ProcessCopyInstruction(std::vector<std::string> line);
+    };
+}
